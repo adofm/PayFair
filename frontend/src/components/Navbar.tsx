cdba03343518f990bfcd94a13e50591d3af7f2e1
s4nj1th <sanjith@tutamail.com>
2025-01-29 14:16:50 +0530

    removed unnecessary files

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+  const isLoggedIn = false;
+  const walletBalance = 100;
+
+  return (
+    <nav className="bg-white shadow-md p-4 flex justify-between items-center">
+        
+    </nav>
+  );
+};
+
+export default Navbar;
